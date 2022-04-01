bb787687d60d09b4af3ac2f89cd01753d525cf55
Bader-Eddine, MESSAOUDI <bader-eddine.messaoudi@cgi.com>
2022-04-01 09:22:32 +0200

    Initializing project

---
+++ WebApp.Predict/Startup.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ML;
+using Microsoft.ML;
+using ImageClassification.WebApp.ML.DataModels;
+using ImageClassification.DataModels;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.Drawing;
+using TensorFlowImageClassification.Controllers;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
+using System.Threading;
+
+namespace ImageClassification.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddControllers();
+            services.AddRazorPages();
+
+            /////////////////////////////////////////////////////////////////////////////
+            // Register the PredictionEnginePool as a service in the IoC container for DI.
+            //
+            services.AddPredictionEnginePool<InMemoryImageData, ImagePrediction>()
+                    .FromFile(Configuration["MLModel:MLModelFilePath"]);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
